e92a1518ad31cfdc3ec3bb839cf9b8f0440b662d
Chad Edwards <chadedwardsofficial@gmail.com>
2024-02-14 01:07:14 -0600

    changed signup form createuser api to the new mutation Create_user

--- Develop/client/src/components/SignupForm.jsx
+++ Develop/client/src/components/SignupForm.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
+import { CREATE_USER } from '../utils/mutations';
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
+
+  const [createUser, { error }] = useMutation(CREATE_USER);
+
+
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
   const [validated] = useState(false);
@@ -28,15 +33,11 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await createUser({
+        variables: userFormData,
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
