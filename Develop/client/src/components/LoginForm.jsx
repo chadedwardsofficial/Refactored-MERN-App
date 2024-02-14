ac2c6955d49f9d93233245415c90379f0ab380ac
Chad Edwards <chadedwardsofficial@gmail.com>
2024-02-14 01:14:42 -0600

    added login mutation to the login form in client side react

--- Develop/client/src/components/LoginForm.jsx
+++ Develop/client/src/components/LoginForm.jsx
@@ -1,11 +1,16 @@
 // see SignupForm.js for comments
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
+
+  const [login, {error}] = useMutation(LOGIN_USER)
+
+
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,19 +31,18 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+        variables: userFormData});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+     
+      
+        const { token, user } = data.loginUser;
+        console.log(user);
+        Auth.login(token);
+      } catch (err) {
+        console.error(err);
+        setShowAlert(true);
       }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
 
     setUserFormData({
       username: '',
